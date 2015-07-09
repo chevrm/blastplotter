6c415a2306e8ddd66eb78062cfb3df3e0d57bdf6
Marc Chevrette <chevrm@gmail.com>
2015-07-09 12:01:12 -0400

    initcommit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# blastplotter.pl
+# blastplotter
+
+blastplotter is a tool to quickly visualize e-value and percent ID of blast output.
+Currently accepted input formats are:
+	Command line:	outfmt 6
+	Web Blast:	Hit Table(text)
+
+Dependancies:
+	perl
+	R
+	ggplot2 (R-package)
+	gridExtra (R-package)
+
+Usage:
+	perl blastplotter.pl example/TX4SESG9015-Alignment.txt
+
+If running the example attached, a file example/TX4SESG9015-Alignment.png should be generated.  If not, ensure that both R package dependancies are installed.
+
+
+Note:  E-Score of n is equivalent to e-value of 1e-n
---
+++ blastplotter.pl
@@ -0,0 +1,70 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+
+my $ifn = shift;
+my $ofn = $ifn;
+$ofn =~ s/^(.+)\..+$/$1\.blpl.tsv/;
+my %h = ();
+open my $ifh, '<', $ifn or die $!;
+my $ncbi = 0;
+while(<$ifh>){
+	chomp;
+	$ncbi = 1 if($_ =~ m/^# RID:/);
+	next if($_ =~ m/^#/);
+	next if($_ =~ m/^(\s+)?$/);
+	my ($query, $hit, $pctpos, $pctid, $alen, $mismatch, $gapopen, $qstart, $qend, $sstart, $send, $evalue, $bitscore) = (undef,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef);
+	if($ncbi == 0){
+		($query, $hit, $pctid, $alen, $mismatch, $gapopen, $qstart, $qend, $sstart, $send, $evalue, $bitscore) = split(/\t/, $_);
+	}else{
+		($query, $hit, $pctid, $pctpos, $alen, $mismatch, $gapopen, $qstart, $qend, $sstart, $send, $evalue, $bitscore) = split(/\t/, $_);
+	}
+	my $escore = 0;
+	if($evalue == 0){
+		$escore = 200;
+	}elsif($evalue =~ m/^([\d\.]+)e-(\d+)$/i){
+		$escore = $2 . '.' . int($1);
+	}
+	if(exists $h{$hit}){
+		if($h{$hit}{'escore'} > $escore){
+			next;
+		}
+	}
+	$h{$hit} = {
+		'escore'	=> $escore,
+		'pid'		=> $pctid,
+	};
+}
+close $ifh;
+open my $ofh, '>', $ofn or die $!;
+print $ofh join("\t", "Hit", "Type", "Score") . "\n";
+foreach(keys %h){
+	print $ofh join("\t", $_, 'E', $h{$_}{'escore'}) . "\n" . join("\t", $_, 'P', $h{$_}{'pid'}) . "\n";
+}
+close $ofh;
+my $png = $ofn;
+$png =~ s/\.blpl\.tsv$/.png/;
+
+## R-Code
+open my $r, '>', 'tmp.R' or die $!;
+# no need to setwd
+print $r "data <- read.table(\"$ofn\", header=TRUE, sep=\"\\t\")\n";
+print $r 'data$Score <- as.numeric(data$Score)' . "\n";
+print $r "e <- subset(data, Type==\"E\")\n";
+print $r "p <- subset(data, Type==\"P\")\n";
+print $r 'e$Rank <- rank(e$Score)/length(e$Score)' . "\n";
+print $r 'p$Rank <- rank(p$Score)/length(p$Score)' . "\n";
+print $r "library(ggplot2)\n";
+print $r 'eplot <- ggplot(e, aes(Rank, Score)) + geom_point(color="deepskyblue3", size=4) + geom_line(color="deepskyblue3", size=1) + ggtitle("E-Score") + labs(y="Score")' . "\n";
+print $r 'pplot <- ggplot(p, aes(Rank, Score)) + geom_point(color="palegreen3", size=4) + geom_line(color="palegreen3", size=1) + ggtitle("Percent ID") + labs(y="Score")' . "\n";
+print $r "library(gridExtra)\n";
+print $r "g <- arrangeGrob(eplot, pplot, ncol=2)\n";
+print $r "ggsave(file=\"$png\", g, dpi=300, height=5, width=10)\n";
+close $r;
+
+## Cleanup
+system("R CMD BATCH tmp.R");
+system("rm tmp.R*");
+system("rm Rplots.pdf") if(-e 'Rplots.pdf');
+system("rm $ofn");
